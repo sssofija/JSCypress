566fbd989c2469ec1bab8c511454d544f2adcfda
sssofija <sofiia.piepponen@gmail.com>
2025-07-22 16:40:09 +0300

    test4_update

--- cypress/e2e/test4.cy.js
+++ cypress/e2e/test4.cy.js
@@ -3,7 +3,14 @@ import '../support/commands/index';
 describe('Register User with existing email', () => {
     it('should not allow registration with existing email', () => {
 
-        cy.registerNewUser(); 
+        cy.openHomePage();
+        cy.goToLoginPage();
+        cy.validUser();
+        cy.get('@newUser').then((user) => {
+            cy.goToLoginPage();
+            cy.logIn(user.email, user.password);
+            cy.contains(`Logged in as ${user.fullName}`).should('be.visible');
+        })
         cy.logOut();
         cy.contains('Login to your account').should('be.visible');
     });
